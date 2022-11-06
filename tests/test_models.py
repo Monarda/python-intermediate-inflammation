4db5b38a75bf9e8f3c9c2b08bb87118fb957d0dd
Ivan Finch <ivan.finch@stfc.ac.uk>
2022-11-06 16:58:07 +0000

    Added test for daily_min

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,3 +29,15 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_daily_min_integers():
+    """ Test that min function words for an array of positive intgers"""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([1, 2])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
